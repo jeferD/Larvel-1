4eae7a8ecfba231a7aa0025ffe1fcb3608a00ea9
Jeferson Delgado <jefersondelgado0918@gmail.com>
2023-02-22 17:27:22 -0500

    Base de datos, migraciones,

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,23 +9,25 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void//crea tabla
     {
+        // users: nombre de la tabla
+        // Blueprint: crea las columnas de la tabla
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id();//integuer unsigned incremend
+            $table->string('name', 150);//varchar 150 caracteres
+            $table->string('email')->unique();//dato unico
+            $table->timestamp('email_verified_at')->nullable();//guardar fechas, al realizar el registro se anula
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();//genera un token de hasta si 100 caracteres
+            $table->timestamps();//almacena fecha y hora que se registra
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down(): void//revierte los cambios, rollback
     {
         Schema::dropIfExists('users');
     }
